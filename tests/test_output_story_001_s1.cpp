d8d55be6e15485d5fb0c38b33900df4ddc6be798
AnaZec <ana.zecns@gmail.com>
2026-04-21 09:21:35 +0200

    #35 refactor(output): OUTPUT-STORY-001-S1 simplify field data to single row

--- tests/test_output_story_001_s1.cpp
+++ tests/test_output_story_001_s1.cpp
@@ -4,10 +4,10 @@
 
 TEST(OUTPUT_STORY_001_S1, PrintSingleAnnotatedFieldWithCorrectHeader) {
     // GIVEN
-    Field field{2, 2, {"1*", "11"}};
+    Field field{1, 2, {"1*"}};
     // WHEN
     std::ostringstream out;
     printField(out, field, 1);
     // THEN
-    EXPECT_EQ(out.str(), "Field #1:\n1*\n11\n");
+    EXPECT_EQ(out.str(), "Field #1:\n1*\n");
 }
