a17fbc7bae9709b503b8522c121a8fb93cfb9f5e
AnaZec <ana.zecns@gmail.com>
2026-04-21 05:56:15 +0200

    #30 refactor(proc): PROC-BE-001.1-S1 count adjacent mines with bounds check - GREEN phase 2

--- src/FieldProcessor.cpp
+++ src/FieldProcessor.cpp
@@ -2,20 +2,21 @@
 
 Field FieldProcessor::process(const Field& field) {
     Field result = field;
-    bool hasMines = false;
-    for (int r = 0; r < field.rows; r++) {
-        for (int c = 0; c < field.cols; c++) {
-            if (field.grid[r][c] == '*') hasMines = true;
-        }
-    }
     for (int r = 0; r < field.rows; r++) {
         for (int c = 0; c < field.cols; c++) {
             if (field.grid[r][c] == '.') {
-                if (hasMines && r == 1 && c == 1) {
-                    result.grid[r][c] = '3';
-                } else {
-                    result.grid[r][c] = '0';
+                int count = 0;
+                for (int dr = -1; dr <= 1; dr++) {
+                    for (int dc = -1; dc <= 1; dc++) {
+                        if (dr == 0 && dc == 0) continue;
+                        int nr = r + dr;
+                        int nc = c + dc;
+                        if (nr >= 0 && nr < field.rows && nc >= 0 && nc < field.cols) {
+                            if (field.grid[nr][nc] == '*') count++;
+                        }
+                    }
                 }
+                result.grid[r][c] = '0' + count;
             }
         }
     }
