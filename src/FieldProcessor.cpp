d56b98dfe9181c3a45350a882a2010ec4b1f1c28
AnaZec <ana.zecns@gmail.com>
2026-04-21 05:49:15 +0200

    #30 refactor(proc): PROC-STORY-001-S3 iterate over field - GREEN phase 2

--- src/FieldProcessor.cpp
+++ src/FieldProcessor.cpp
@@ -1,5 +1,13 @@
 #include "FieldProcessor.hpp"
 
 Field FieldProcessor::process(const Field& field) {
-    return {2, 2, {"00", "00"}};
+    Field result = field;
+    for (int r = 0; r < field.rows; r++) {
+        for (int c = 0; c < field.cols; c++) {
+            if (field.grid[r][c] == '.') {
+                result.grid[r][c] = '0';
+            }
+        }
+    }
+    return result;
 }
