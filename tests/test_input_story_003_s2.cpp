61c78e18a90ba94aa7bfece4452a848d79b71830
AnaZec <ana.zecns@gmail.com>
2026-04-21 16:43:17 +0200

    #37 test(input): INPUT-STORY-003-S2 output blocks correspond to input fields

---
+++ tests/test_input_story_003_s2.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <cstdio>
+#include <string>
+
+TEST(INPUT_STORY_003_S2, OutputBlocksCorrespondToInputFields) {
+    // GIVEN - stdin contains two fields with different dimensions
+    FILE* pipe = popen("printf '1 1\n.\n2 2\n**\n**\n0 0\n' | ./minesweeper", "r");
+    ASSERT_NE(pipe, nullptr);
+
+    // WHEN - the full pipeline runs
+    std::string output;
+    char buf[256];
+    while (fgets(buf, sizeof(buf), pipe)) output += buf;
+    pclose(pipe);
+
+    // THEN - first output block reflects first field, second reflects second field
+    EXPECT_NE(output.find("Field #1:\n0\n"), std::string::npos);
+    EXPECT_NE(output.find("Field #2:\n**\n**\n"), std::string::npos);
+}
