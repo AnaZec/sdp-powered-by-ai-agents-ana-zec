8ef83f7fa736ba99c4835753903676b210806412
AnaZec <ana.zecns@gmail.com>
2026-04-21 06:37:03 +0200

    #31 refactor(output): OUTPUT-BE-001.1-S1 iterate over grid rows - GREEN phase 3

--- src/OutputFormatter.cpp
+++ src/OutputFormatter.cpp
@@ -1,5 +1,8 @@
 #include "OutputFormatter.hpp"
 
 void printField(std::ostream& out, const Field& field, int index) {
-    out << "Field #" << index << ":\n1*\n11\n";
+    out << "Field #" << index << ":\n";
+    for (const auto& row : field.grid) {
+        out << row << "\n";
+    }
 }
