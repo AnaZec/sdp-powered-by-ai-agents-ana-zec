d1631aa4e6d8e8ea3fad2b5e810072a37bfab794
AnaZec <ana.zecns@gmail.com>
2026-04-21 15:58:47 +0200

    #37 refactor(output): OUTPUT-BE-003.1-S1 improve structure and use distinct field data

--- tests/test_output_be_003_1.cpp
+++ tests/test_output_be_003_1.cpp
@@ -3,15 +3,16 @@
 #include "OutputFormatter.hpp"
 
 TEST(OUTPUT_BE_003_1_S1, BlankLinePrecedesAllFieldsExceptFirst) {
-    // GIVEN - printField is called with index 1 and then with index 2
-    Field field{1, 1, {"0"}};
+    // GIVEN - two fields to be printed with indices 1 and 2
+    Field field1{1, 1, {"0"}};
+    Field field2{1, 1, {"1"}};
     std::ostringstream out;
 
     // WHEN - both calls complete
-    printField(out, field, 1);
-    printField(out, field, 2);
+    printField(out, field1, 1);
+    printField(out, field2, 2);
 
     // THEN - output for index 1 has no leading blank line
     // THEN - output for index 2 is preceded by exactly one blank line
-    EXPECT_EQ(out.str(), "Field #1:\n0\n\nField #2:\n0\n");
+    EXPECT_EQ(out.str(), "Field #1:\n0\n\nField #2:\n1\n");
 }
