ca5cab64de926dc69de50bda816080ee258b684c
AnaZec <ana.zecns@gmail.com>
2026-04-21 06:36:12 +0200

    #31 refactor(output): OUTPUT-BE-001.1-S1 use index parameter - GREEN phase 2

--- src/OutputFormatter.cpp
+++ src/OutputFormatter.cpp
@@ -1,5 +1,5 @@
 #include "OutputFormatter.hpp"
 
 void printField(std::ostream& out, const Field& field, int index) {
-    out << "Field #2:\n1*\n11\n";
+    out << "Field #" << index << ":\n1*\n11\n";
 }
