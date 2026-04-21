8ecd387c0f9a396c3a9b9bf4a6974ca3cf3b73cf
AnaZec <ana.zecns@gmail.com>
2026-04-21 16:28:26 +0200

    #37 test(input): INPUT-INFRA-003.1-S1 multi-field test executes inside Docker

---
+++ tests/test_input_infra_003_1.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include "InputParser.hpp"
+
+TEST(INPUT_INFRA_003_1_S1, MultiFieldTestExecutesInsideDocker) {
+    // GIVEN - input with two fields via stringstream
+    std::istringstream input("1 1\n.\n2 2\n..\n..\n0 0\n");
+
+    // WHEN - parseFields is called
+    auto fields = parseFields(input);
+
+    // THEN - multi-field parsing works correctly
+    ASSERT_EQ(fields.size(), 2);
+    EXPECT_EQ(fields[0].rows, 1);
+    EXPECT_EQ(fields[1].rows, 2);
+}
