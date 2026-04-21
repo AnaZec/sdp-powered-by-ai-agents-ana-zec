edfb0fb9a564140e3e7e75fcf914a66c129c125a
AnaZec <ana.zecns@gmail.com>
2026-04-21 15:00:49 +0200

    #37 refactor(input): INPUT-BE-002.1-S1 assert grid content and terminator not included as row

--- tests/test_input_be_002_1.cpp
+++ tests/test_input_be_002_1.cpp
@@ -3,11 +3,13 @@
 #include "InputParser.hpp"
 
 TEST(INPUT_BE_002_1_S1, ExitLoopOnZeroDimensionHeader) {
-    // GIVEN
+    // GIVEN - one valid field followed by terminator "0 0"
     std::istringstream input("2 2\n..\n..\n0 0\n");
     // WHEN
     auto fields = parseFields(input);
-    // THEN
+    // THEN - only the pre-terminator field is returned, terminator not added as row
     ASSERT_EQ(fields.size(), 1u);
-    EXPECT_EQ(fields[0].grid.size(), 2u);
+    ASSERT_EQ(fields[0].grid.size(), 2u);
+    EXPECT_EQ(fields[0].grid[0], "..");
+    EXPECT_EQ(fields[0].grid[1], "..");
 }
