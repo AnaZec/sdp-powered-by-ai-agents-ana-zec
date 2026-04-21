60dc24275ae0a47fbce40709155a084c6fc5eba3
AnaZec <ana.zecns@gmail.com>
2026-04-21 08:47:37 +0200

    #35 refactor(output): OUTPUT-FE-001.1-S1 remove unnecessary braces in printField

--- src/OutputFormatter.cpp
+++ src/OutputFormatter.cpp
@@ -1,11 +1,8 @@
 #include "OutputFormatter.hpp"
 
 void printField(std::ostream& out, const Field& field, int index) {
-    if (index > 1) {
-        out << "\n";
-    }
+    if (index > 1) out << "\n";
     out << "Field #" << index << ":\n";
-    for (const auto& row : field.grid) {
+    for (const auto& row : field.grid)
         out << row << "\n";
-    }
 }
